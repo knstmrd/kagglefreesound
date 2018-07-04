5ac012678cf56b41374b961d265188028986a958
George Oblapenko <kunstmord@kunstmord.com>
2018-07-04 15:22:29 +0300

    Improved CV logging

--- writelogs.py
+++ writelogs.py
@@ -11,12 +11,14 @@ def write_list(fname: str, list_to_write: List[str], mode: str='a', ending=None)
 
 
 def log_cv_results(classifier, score: float, n_features, settings: Dict):
-    log = ['Features files: ' + ','.join(settings['features_files'])]
-    log.append('Exclusion files: ' + ','.join(settings['exclude_features_files']))
-    log.append('Total # of used features ' + str(n_features))
-    log.append(str(classifier))
-    log.append('Score: ' + str(score))
-    write_list('logs/cv/' + settings['method'] + '/cv.log', log, 'a', '\n\n')
+    log = ['Score: ' + str(score)]
+    log += ['Classifier: ' + str(classifier)]
+    log += ['Feature file: ' + settings['features_file']]
+    log += ['Correlated features files: ' + '_'.join(settings['correlated_features_files'])]
+    log += ['Unimportant features files: ' + '_'.join(settings['unimportant_features_files'])]
+    log += ['Total # of used features ' + str(n_features)]
+
+    write_list('logs/cv/' + settings['method'] + '_cv.log', log, 'a', '\n\n')
 
 
 def log_submission(classifier, n_features, settings: Dict):
